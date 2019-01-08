82d295f939423317aff6ec5564b678987672a3ca
polyglotarist <majid.samir2@gmail.comgit config --global user.email majid.samir2@gmail.com>
2019-01-08 17:06:32 -0600

    class assignment

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+car_1 = {:make => "honda", :model => "civic", :year => 2000}
+car_2 = {:make => "toyota", :model => "corolla", :year => 2010}
+car_3 = {:make => "ford", :model => "focus", :year => 2014}
+
+puts car_1[:make]
